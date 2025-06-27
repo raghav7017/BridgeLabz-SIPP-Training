eb458a3cd985e6ddbeb4a846b9223b7e75313926
raghav7017 <raghavmaheshwari422@gmail.acom>
2025-06-27 09:53:28 +0530

    first commit

---
+++ ControlFlow/oddEven.java
@@ -0,0 +1,26 @@
+package ControlFlow;
+import java.util.Scanner;
+
+public class oddEven {
+    
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a natural number: ");
+        int number = sc.nextInt();
+
+        if (number < 1) {
+            System.out.println("Please enter a natural number greater than 0.");
+           
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is an even number");
+            } else {
+                System.out.println(i + " is an odd number");
+            }
+        }
+        sc.close();
+    }
+}
---
+++ ControlFlow/powerWhile.java
@@ -0,0 +1,18 @@
+package ControlFlow;
+import java.util.Scanner;
+
+public class powerWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int power = sc.nextInt();
+        int result = 1;
+        int counter = 0;
+        while (counter < power) {
+            result *= number;
+            counter++;
+        }
+        System.out.println(result);
+        sc.close();
+    }
+}
